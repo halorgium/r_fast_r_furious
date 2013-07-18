320babe57399994afbad627a5a0ab11e815ca710
Erik Michaels-Ober <sferik@gmail.com>
2013-07-18 05:26:10 +0200

    Revert "Add support for offline checks"
    This reverts commit 4cc4e121f2f16882b71fe5215d001c61369c961b.

--- lib/r_fast_r_furious.rb
+++ lib/r_fast_r_furious.rb
@@ -1,7 +1,20 @@
+require "v8"
+require "net/http"
+require "uri"
+
 module RFastRFurious
-  REGEX = /^((T(he)(?=.*the)|2(?=.* 2)) )?Fast (Five|(((and t\3)|\2(?=.*s$)|&(?!.*:)) Furious(( (6|7))|: Tokyo Drift)?))$/
-  def check(string)
-    !!REGEX.match(string)
+  SOURCE = "https://raw.github.com/alunny/r_fast_r_furious/master/fast.js"
+  def check(string, url = SOURCE)
+    uri = URI.parse(SOURCE)
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    request = Net::HTTP::Get.new(uri.request_uri)
+    response = http.request(request)
+    content = response.body
+    cxt = V8::Context.new
+    cxt.eval(content, "fast.js")
+    cxt["r_fast_r_furious"].call(string)
   end
   module_function :check
 end
