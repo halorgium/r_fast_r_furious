5300a21314d2e0e6aeb92c372735a069a17e5e0d
Erik Michaels-Ober <sferik@gmail.com>
2013-07-18 05:35:52 +0200

    Online by default; fallback to offline

--- lib/r_fast_r_furious.rb
+++ lib/r_fast_r_furious.rb
@@ -4,6 +4,14 @@ require "uri"
 
 module RFastRFurious
   SOURCE = "https://raw.github.com/alunny/r_fast_r_furious/master/fast.js"
+  REGEX = /^((T(he)(?=.*the)|2(?=.* 2)) )?Fast (Five|(((and t\3)|\2(?=.*s$)|&(?!.*:)) Furious(( (6|7))|: Tokyo Drift)?))$/
+  OFFLINE_ERRORS = [SocketError, Timeout::Error]
+
+  def offline_check(string)
+    !!REGEX.match(string)
+  end
+  module_function :offline_check
+
   def check(string, url = SOURCE)
     uri = URI.parse(SOURCE)
     http = Net::HTTP.new(uri.host, uri.port)
@@ -15,6 +23,8 @@ module RFastRFurious
     cxt = V8::Context.new
     cxt.eval(content, "fast.js")
     cxt["r_fast_r_furious"].call(string)
+  rescue *OFFLINE_ERRORS
+    offline_check(string)
   end
   module_function :check
 end
